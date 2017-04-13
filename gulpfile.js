8b62cdc845c90263bf755dee65f51ffd1ab759ce
Pontus Wallberg <pontus.wallberg@gmail.com>
2017-04-13 22:08:42 +0200

    simple gulp setup

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function(){
+	return gulp.src('scss/test.scss')
+		.pipe(sass()) // Converts Sass to CSS with gulp-sass
+		.pipe(gulp.dest('css'));
+});
