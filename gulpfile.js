0e546098a8c6c7b772cfddf9bd73a7bab655c220
Pontus Wallberg <pontus.wallberg@gmail.com>
2017-04-14 11:04:45 +0200

    renamed test -> funke

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
-	return gulp.src('scss/test.scss')
+	return gulp.src('scss/funke.scss')
 		.pipe(sass()) // Converts Sass to CSS with gulp-sass
 		.pipe(autoprefixer({
 			browsers: ['last 2 versions'],
