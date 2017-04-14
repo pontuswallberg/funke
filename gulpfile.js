840e70c16ad2e7978666da3aebe1a8813fe75ad7
Henrik Sjööh <henriiik@gmail.com>
2017-04-14 11:53:49 +0200

    add csso and watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,24 @@
 var gulp = require('gulp');
 
 var autoprefixer = require('gulp-autoprefixer');
+var csso = require('gulp-csso');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 
+let srcFile = 'scss/funke.scss';
+
+gulp.task('watch', function () {
+	gulp.watch(srcFile, ['sass']);
+});
 
 gulp.task('sass', function () {
-	return gulp.src('scss/funke.scss')
+	return gulp.src(srcFile)
 		.pipe(sourcemaps.init())
 		.pipe(sass()) // Converts Sass to CSS with gulp-sass
 		.pipe(autoprefixer({
 			browsers: ['last 2 versions'],
 		})) // Autoprefix CSS with gulp-autoprefixer
+		.pipe(csso())
 		.pipe(sourcemaps.write())
 		.pipe(gulp.dest('css'))
 		.pipe(gulp.dest('docs/css'));
