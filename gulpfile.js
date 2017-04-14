a12103ad9f394bb3d1453a2414f7d99664d11dd2
Henrik Sjööh <henriiik@gmail.com>
2017-04-14 12:07:32 +0200

    add http server

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,16 @@ var autoprefixer = require('gulp-autoprefixer');
 var csso = require('gulp-csso');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var http = require('http-server');
 
 let srcFile = 'scss/funke.scss';
 
 gulp.task('watch', function () {
+	let server = http.createServer({
+		root: './docs'
+	});
+	server.listen(8000);
+	console.log('Listening on http://localhost:8000')
 	gulp.watch(srcFile, ['sass']);
 });
 
