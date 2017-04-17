6010164acffdaffd82f8dd150a6af2c27d40cc75
Henrik Sjööh <henriiik@gmail.com>
2017-04-17 20:00:54 +0200

    add highlight js

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gutil = require('gulp-util');
 
 var autoprefixer = require('gulp-autoprefixer');
 var csso = require('gulp-csso');
+var highlight = require('gulp-highlight');
 var http = require('http-server');
 var mustache = require("gulp-mustache");
 var sass = require('gulp-sass');
@@ -18,13 +19,13 @@ gulp.task('server', function () {
 	gutil.log('Listening on http://localhost:8000/funke')
 });
 
-gulp.task('watch', function () {
+gulp.task('watch', ['sass', 'mustache'], function () {
 	gulp.watch('scss/**/*.scss', ['sass']);
 	gulp.watch('templates/**/*.html', ['mustache']);
 });
 
 gulp.task('sass', function () {
-	return gulp.src('scss/funke.scss')
+	return gulp.src('scss/{funke,hljs}.scss')
 		.pipe(sourcemaps.init())
 		.pipe(sass().on('error', sass.logError)) /// Converts Sass to CSS with gulp-sass
 		.pipe(autoprefixer({
@@ -38,8 +39,7 @@ gulp.task('sass', function () {
 
 gulp.task('mustache', function () {
 	return gulp.src('./templates/*.html')
-		.pipe(mustache({
-			msg: 'hello'
-		}))
+		.pipe(mustache())
+		.pipe(highlight())
 		.pipe(gulp.dest('docs'));
 })
